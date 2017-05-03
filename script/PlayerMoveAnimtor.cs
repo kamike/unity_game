0d0827a3effc0bfb2b3b74e2c97059b7e1600a23
kamike <751190264@qq.com>
2017-05-03 18:15:30 +0800

    init msg

---
+++ script/PlayerMoveAnimtor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMoveAnimtor : MonoBehaviour {
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        anim.SetFloat("x", horizontal);
+        anim.SetFloat("y", vertical);
+        transform.Translate(horizontal,0,0);
+
+        //anim.SetFloat("y", m_TurnAmount, 0.1f, Time.deltaTime);
+    }
+
+ 
+  
+   
+}
